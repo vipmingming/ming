0e628a315d81717976788af37a90cb3e3572029a
vipming <44264950+vipming@users.noreply.github.com>
2018-12-02 14:17:47 +0800

    Update InstallNET.sh

--- InstallNET.sh
+++ InstallNET.sh
@@ -46,7 +46,7 @@ while [[ $# -ge 1 ]]; do
       linuxdists='ubuntu'
       tmpDIST="$1"
       shift
-      ;;
+      
     -c|--centos)
       shift
       linuxdists='centos'
@@ -55,7 +55,7 @@ while [[ $# -ge 1 ]]; do
       ;;
     -dd|--image)
       shift
-      ddMode='1'
+      ddMode='1
       tmpURL="$1"
       shift
       ;;
@@ -65,6 +65,7 @@ while [[ $# -ge 1 ]]; do
       shift
       ;;
     -i|--interface)
+    
       shift
       interface="$1"
       shift
@@ -73,7 +74,7 @@ while [[ $# -ge 1 ]]; do
       shift
       ipAddr="$1"
       shift
-      ;;
+      ;
     --ip-mask)
       shift
       ipMask="$1"
@@ -82,7 +83,7 @@ while [[ $# -ge 1 ]]; do
     --ip-gate)
       shift
       ipGate="$1"
-      shift
+      shif
       ;;
     --loader)
       shift
@@ -91,7 +92,7 @@ while [[ $# -ge 1 ]]; do
     --prefer)
       shift
       tmpPrefer="$1"
-      shift
+      shif
       ;;
     -a|--auto)
       shift
@@ -246,7 +247,82 @@ fi
 if [[ -n "$tmpPrefer" ]]; then
   PreferOption="$(echo "$tmpPrefer" |sed 's/[[:space:]]*//g')"
 fi
- 
+ elif [[ "$linuxdists" == 'centos' ]]; then
+cat >/tmp/boot/ks.cfg<<EOF
+#platform=x86, AMD64, or Intel EM64T
+firewall --enabled --ssh
+install
+url --url="http://$DISTMirror/$DIST/os/$VER/"
+rootpw --iscrypted $myPASSWORD
+auth --useshadow --passalgo=sha512
+firstboot --disable
+lang en_US
+keyboard us
+selinux --disabled
+logging --level=info
+reboot
+text
+unsupported_hardware
+vnc
+skipx
+timezone --isUtc Asia/Hong_Kong
+#ONDHCP network --bootproto=dhcp --onboot=on
+#NODHCP network --bootproto=static --ip=$IPv4 --netmask=$MASK --gateway=$GATE --nameserver=8.8.8.8 --onboot=on
+bootloader --location=mbr --append="rhgb quiet crashkernel=auto"
+zerombr
+clearpart --all --initlabel 
+autopart
+
+%packages
+@base
+%end
+
+%post --interpreter=/bin/bash
+rm -rf /root/anaconda-ks.cfg
+rm -rf /root/install.*log
+%end
+
+EOF
+
+[[ "$setNet" == '0' ]] && [[ "$AutoNet" == '1' ]] && {
+  sed -i 's/#ONDHCP\ //g' /tmp/boot/ks.cfg
+} || {
+  sed -i 's/#NODHCP\ //g' /tmp/boot/ks.cfg
+}
+[[ "$UNKNOWHW" == '1' ]] && sed -i 's/^unsupported_hardware/#unsupported_hardware/g' /tmp/boot/ks.cfg
+[[ "$(echo "$DIST" |grep -o '^[0-9]\{1\}')" == '5' ]] && sed -i '0,/^%end/s//#%end/' /tmp/boot/ks.cfg
+fi
+
+find . | cpio -H newc --create --verbose | gzip -9 > /boot/initrd.img;
+rm -rf /tmp/boot;
+}
+
+[[ "$inVNC" == 'y' ]] && {
+  sed -i '$i\\n' $GRUBDIR/$GRUBFILE
+  sed -i '$r /tmp/grub.new' $GRUBDIR/$GRUBFILE
+  echo -e "\n\033[33m\033[04mIt will reboot! \nPlease look at VNC! \nSelect\033[0m\033[32m Install OS [$DIST $VER] \033[33m\033[4mto install system.\033[04m\n\n\033[31m\033[04mThere is some information for you.\nDO NOT CLOSE THE WINDOW! \033[0m\n"
+  echo -e "\033[35mIPv4\t\tNETMASK\t\tGATEWAY\033[0m"
+  echo -e "\033[36m\033[04m$IPv4\033[0m\t\033[36m\033[04m$MASK\033[0m\t\033[36m\033[04m$GATE\033[0m\n\n"
+
+  read -n 1 -p "Press Enter to reboot..." INP
+  [[ "$INP" != '' ]] && echo -ne '\b \n\n';
+}
+
+chown root:root $GRUBDIR/$GRUBFILE
+chmod 444 $GRUBDIR/$GRUBFILE
+
+if [[ "$loaderMode" == "0" ]]; then
+  sleep 3 && reboot >/dev/null 2>&1
+else
+  rm -rf "$HOME/loader"
+  mkdir -p "$HOME/loader"
+  cp -rf "/boot/initrd.img" "$HOME/loader/initrd.img"
+  cp -rf "/boot/vmlinuz" "$HOME/loader/vmlinuz"
+  [[ -f "/boot/initrd.img" ]] && rm -rf "/boot/initrd.img"
+  [[ -f "/boot/vmlinuz" ]] && rm -rf "/boot/vmlinuz"
+  echo && ls -AR1 "$HOME/loader"
+fi
+
 if [[ -z "$PreferOption" ]]; then
   PreferOption='current';
 fi
